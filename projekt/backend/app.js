1cd53133e9226d2a043ff0d058a094260dae7eca
Marcin Kotowski <marcin.kotowski02@gmail.com>
2025-06-13 15:14:13 +0200

    Move CORS url to env

--- projekt/backend/app.js
+++ projekt/backend/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORSURL || "http://localhost:5173",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
